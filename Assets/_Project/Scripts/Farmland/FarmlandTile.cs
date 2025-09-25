f031b9e6d86fba680d2afec1a59f53fa0d36001a
Jan <janfrancoherrera@gmail.com>
2025-09-25 16:10:28 +0100

    Crop Highlight
    # Description
    Added a shader and a material to show a fresnel effect on the crops that are currently selected.
    
    Modified the dust effect to lerp the particles into existence.
    
    Farmland tiles now update the tint and the renderer material of the plant based on selection and whether it is affected by the dust.

--- Assets/_Project/Scripts/Farmland/FarmlandTile.cs
+++ Assets/_Project/Scripts/Farmland/FarmlandTile.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-using UnityEngine.Events;
+using Utils;
 
 public class FarmlandTile : MonoBehaviour, ILookHoverer
 {
@@ -11,11 +11,28 @@ public class FarmlandTile : MonoBehaviour, ILookHoverer
     private GameObject _plantRoot;
     [SerializeField]
     private ParticleSystem _dustEffect;
+    [SerializeField]
+    private Renderer _plantRenderer;
+    [SerializeField]
+    private Material _highlightMaterial;
 
     [SerializeField, Tooltip("Health of the plant before it withers, each health point represents one second.")]
     private float _health = 60.0f;
 
+    private Material _defaultMaterial;
     private bool _isAffected = false;
+    private Color _tint;
+
+    private void Awake()
+    {
+        this.AssertReference(_plantRoot);
+        this.AssertReference(_dustEffect);
+        this.AssertReference(_plantRenderer);
+        this.AssertReference(_highlightMaterial);
+
+        _defaultMaterial = _plantRenderer.material;
+        _tint = _plantRenderer.material.color;  
+    }
 
     private void Update()
     {
@@ -36,6 +53,7 @@ public class FarmlandTile : MonoBehaviour, ILookHoverer
             _isAffected = true;
             _dustEffect.Play();
             OnDustAffected?.Invoke(this);
+            SetPlantTint(Color.gray); // Change color to indicate dust effect
         }
     }
 
@@ -61,12 +79,20 @@ public class FarmlandTile : MonoBehaviour, ILookHoverer
     {
         _isAffected = false;
         _dustEffect.Stop();
+        SetPlantTint(Color.white);
+    }
+
+    private void SetPlantTint(Color color)
+    {
+        _tint = color;
+        _plantRenderer.material.color = _tint;
     }
 
     #region ILookHoverer Interface
     public void OnLookHoverEnter()
     {
-        
+        _plantRenderer.material = _highlightMaterial; 
+        _plantRenderer.material.color = _tint; // Maintain the current tint
     }
 
     public void OnLookHoverInteract()
@@ -76,7 +102,8 @@ public class FarmlandTile : MonoBehaviour, ILookHoverer
 
     public void OnLookHoverExit()
     {
-        
+        _plantRenderer.material = _defaultMaterial;
+        _plantRenderer.material.color = _tint; // Maintain the current tint
     }
 
     #endregion
